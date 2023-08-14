be212e1d403b8de028dd5a8f104b96b156e8625c
Upper Walkman <UpperCIMC@outlook.com>
2023-08-13 19:42:08 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeros = "5 23";
+            string[] enteros = numeros.Split(' ');
+            string resultado = string.Empty;
+            int a = int.Parse(enteros[0]);
+            int b = int.Parse(enteros[1]);
+            for (int i = a; i <= b; i++)
+            {
+                if(i % a == 0)
+                {
+                    resultado += $"{i} ";
+                }
+            }
+            Console.WriteLine(resultado);
+        }
+    }
+}
